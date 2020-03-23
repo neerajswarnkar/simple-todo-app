100bcd85f7c86e3b3df96dcff7bac39720b72b57
neerajswarnkar <neeraj.swarnkar51@gmail.com>
2020-03-24 02:21:34 +0530

    Updates

---
+++ precache-manifest.25e3be59e624136298a1ab5371337563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d08c293a25a6d7d9d2b4aa14d98fd4",
+    "url": "//simple-todo-app/index.html"
+  },
+  {
+    "revision": "ccaf56c579dc184e3fc5",
+    "url": "//simple-todo-app/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "3c4fd517ae662e14dacc",
+    "url": "//simple-todo-app/static/js/2.d39d414a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//simple-todo-app/static/js/2.d39d414a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccaf56c579dc184e3fc5",
+    "url": "//simple-todo-app/static/js/main.f752d4ee.chunk.js"
+  },
+  {
+    "revision": "f2b402768f2ea5a9d12c",
+    "url": "//simple-todo-app/static/js/runtime-main.11998b8d.js"
+  }
+]);
